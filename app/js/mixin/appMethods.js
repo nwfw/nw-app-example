01bb186397fa2f04fc73bc953bb1963513fc7dc8
Dino Ivankov <dinoivankov@gmail.com>
2017-07-03 19:07:58 +0200

    Add formatTime(Normalize) app helper methods. Remove js/lib (since it's being loaded from appwrapper)

--- app/js/mixin/appMethods.js
+++ app/js/mixin/appMethods.js
@@ -14,8 +14,14 @@ var MixinAppMethods  = {
         formatDate: function (date, options, includeTime) {
             return _appWrapper.getHelper('format').formatDate(date, options, includeTime);
         },
+        formatTime: function (date, options, includeDate) {
+            return _appWrapper.getHelper('format').formatTime(date, options, includeDate);
+        },
         formatDateNormalize: function (date, options, includeTime, omitSeconds) {
             return _appWrapper.getHelper('format').formatDateNormalize(date, options, includeTime, omitSeconds);
+        },
+        formatTimeNormalize: function (date, options, includeDate, omitSeconds) {
+            return _appWrapper.getHelper('format').formatTimeNormalize(date, options, includeDate, omitSeconds);
         },
         addZeros: function (value, maxLength) {
             return _appWrapper.getHelper('format').addZeros(value, maxLength);
